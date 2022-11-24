fe31df738445a092f9339552304a4a65eab490d7
Evans Bestmann <42318259+evansbestmann@users.noreply.github.com>
2022-11-24 10:19:53 +0100

    Update settings.py

--- clientside/settings.py
+++ clientside/settings.py
@@ -144,8 +144,8 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'#cl
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-DISABLE_COLLECTSTATIC = 0
+STATIC_ROOT = "https://clientsideonrailway-production.up.railway.app/staticfiles/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
