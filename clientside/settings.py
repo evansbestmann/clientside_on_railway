9e485c81fe42fc68ff4d6dd5a37abd14e2b1fb6c
Evans Bestmann <42318259+evansbestmann@users.noreply.github.com>
2022-11-24 09:01:08 +0100

    Update settings.py

--- clientside/settings.py
+++ clientside/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'cloudinary',
     'client',
-    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
