ae040fd990411258e85d2a52b7bd78c838ee215e
evansbestmann <42318259+evansbestmann@users.noreply.github.com>
2022-12-08 14:27:52 +0100

    final dep10

--- clientside/settings.py
+++ clientside/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'client.LoginCheckMiddleWare.LoginCheckMiddleWare',
 ]
 
 ROOT_URLCONF = 'clientside.urls'
