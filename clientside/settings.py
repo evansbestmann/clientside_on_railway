184a6b3de8f5d32f0423fea91d7604bed6f31efb
evansbestmann <42318259+evansbestmann@users.noreply.github.com>
2022-12-15 07:54:29 +0100

    final dep11

--- clientside/settings.py
+++ clientside/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY="django-insecure-o_(8gg3ifh8mt6z@!5+&4ushdbipo!zq8p6o46uxpqy=u=-y$b"
+SECRET_KEY=os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
